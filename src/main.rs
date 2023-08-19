598de9018071e8bbe457e0a67999909394f4648f
sicarius <sicarius97@protonmail.com>
2023-08-19 01:34:23 +0000

    added polygon maker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(ShapePlugin)
+        .add_systems(Startup, setup_system)
+        .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::RegularPolygon {
+        sides: 6,
+        feature: shapes::RegularPolygonFeature::Radius(200.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shape),
+            ..default()
+        },
+        Fill::color(Color::WHITE),
+        Stroke::new(Color::BLACK, 10.0),
+    ));
 }
